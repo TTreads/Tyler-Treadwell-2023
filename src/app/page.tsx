e126dba8178d3ad6fb85f45f5362fb5542a351e3
Tyler Treadwell <tyler@ttrds.com>
2023-08-17 20:42:24 -0400

    Welcome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,157 @@
-import Image from 'next/image'
+'use client'
+import React, { useEffect } from 'react'
+import tyler from '@/app/tyler.jpg'
+import { motion } from "framer-motion"
+
+const Jobs: Job[] = [{
+  title: `Tech Consulting`,
+  companyName: `Freelance`,
+  location: `Remote`,
+  date: `2022 — Now`,
+  link: `https://tylertreadwell.com`,
+}, {
+
+  title: `UI Designer`,
+  companyName: `Broadridge`,
+  location: `Edgewood, NY`,
+  date: `2019 — 2022`,
+  link: `https://www.broadridge.com/`,
+}, {
+  title: `Founder`,
+  companyName: `Zimute Media Inc`,
+  location: `Deer Park, NY`,
+  date: `2018 — 2022`,
+  link: `http://zimute.com/`,
+
+},
+{
+  title: `Full Stack Web Engineer`,
+  companyName: `Event Horizon Travel`,
+  location: `Remote`,
+  date: `2018 — 2019`,
+  link: `https://eventhorizontravel.com/`,
+},
+{
+  title: `Freelance`,
+  companyName: `TTreads Designs`,
+  location: `Remote`,
+  date: `2014 — 2019`,
+  link: `https://tylertreadwell.com`,
+},
+]
+
+interface Job {
+  title: string;
+  companyName: string;
+  location: string;
+  date?: string;
+  link?: string;
+}
 
 export default function Home() {
+  const [Year, setYear] = React.useState(0)
+  const [FadeDuration, setFadeDuration] = React.useState(0)
+
+  useEffect(() => {
+    let date = new Date()
+    let year = date.getFullYear();
+    setYear(year)
+
+    let fadeDur = Jobs.length
+    setFadeDuration(fadeDur)
+  })
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <header className="max-w-[100vw] min-h-[50px]"></header>
+      <main className='p-5 min-h-[80vh]'>
+        <div className="max-w-[540px] m-auto md:p-5 pt-0 ">
+
+          <motion.div
+            initial={{ opacity: 0, y: 20 }} // Initial state (hidden)
+            animate={{ opacity: 1, y: 0 }}  // Animation to apply
+            transition={{ duration: 0.5 }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="flex items-center gap-x-6">
+              <img className="h-[92px] w-[92px] rounded-full" src={tyler.src} alt="Tyler Treadwell" />
+
+              <div>
+                <h3 className="text-[24px] font-bold leading-7 tracking-tight text-gray-900">Tyler Treadwell</h3>
+                <p className="text-sm font-normal leading-6 text-[#555]">UI Engineer in New York, <span className="text-[#8b8b8b]">He/Him</span></p>
+              </div>
+
+            </div>
+
+
+            <div className='mt-8'>
+
+              <h3 className='text-[14px]'>About</h3>
+              <p>I'm ui engineer, optimist, and entrepreneur. I currently work as contract web consultant. Helping founders build end to end SaaS solutions for their clients.</p>
+            </div>
+          </motion.div>
+          <div className='mt-8'>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }} // Initial state (hidden)
+              animate={{ opacity: 1, y: 0 }}  // Animation to apply
+              transition={{ duration: 0.5 }}
+            >
+              <h3 className='text-[14px]'>Work Experience</h3>
+            </motion.div>
+            {Jobs.map((job, index) => <>
+
+              {
+
+                <motion.div
+                  key={index}
+                  initial={{ opacity: 0, y: 20 }} // Initial state (hidden)
+                  animate={{ opacity: 1, y: 0 }}  // Animation to apply
+                  transition={{ duration: 0.5, delay: index * 0.3 }} // Duration and delay
+                >
+                  <ExperienceItem Job={job} />
+                </motion.div>
+              }
+            </>)}
+          </div>
+
         </div>
-      </div>
+      </main>
+      <motion.div
+        key={FadeDuration}
+        initial={{ opacity: 0, y: 20 }} // Initial state (hidden)
+        animate={{ opacity: 1, y: 0 }}  // Animation to apply
+        transition={{ duration: 0.5, delay: FadeDuration * 0.3 }} // Duration and delay
+      >
+        <footer className='max-w-[540px] m-auto'>
+          <div className='p-5'>
+            <div className="text-[12px]">Connect on <a target="_blank" className='underline' href='https://www.linkedin.com/in/ttreads/'>Linkedin</a></div>
+            <div className='flex-col'><h3 className="text-[20px] font-bold tracking-tight text-gray-900 leading-tight">Tyler <br /> Treadwell</h3></div>
+            <div className='md:flex justify-between'>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+              <div className='flex-col text-[12px] mt-2 text-left'>
+                <p>Find me on <a target="_blank" className='underline' href="https://dribbble.com/ttreads">Dribbble</a></p>
+                <p>Browse my repos on <a target="_blank" className='underline' href="https://github.com/ttreads">Github</a></p>
+              </div>
+              <div className='flex-col text-[12px] mt-2 md:text-right'>
+                <p>Built using <a target='_blank' className='underline' href='https://nextjs.org/?via=TylerTreadwell'>NextJS</a></p>
+                <p className='text-[12px]'>&copy; {Year} All Rights Reserved</p>
+              </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            </div>
+          </div>
+        </footer>
+      </motion.div>
+    </>
   )
 }
+function ExperienceItem(props: any) {
+  console.log(props.Job)
+  return <div className="experience_item mt-5 md:flex justify-left gap-8">
+    <div className="experience_date">
+      <small className='text-[#8b8b8b]'>{props.Job.date}</small>
+    </div>
+    <div className="job align-left">
+      <a className="flex align-center gap-1" target={props.Job.link ? "_blank" : "none"} href={props.Job.link ? props.Job.link : "#"}> <span className='text-[14px]'>{props.Job.title} at {props.Job.companyName}</span><span style={{ whiteSpace: "nowrap" }}><svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z" fill="#111"></path></svg></span></a> <small className='text-[14px] text-[#8b8b8b]'>{props.Job.location}</small>
+    </div>
+  </div>
+}
+
