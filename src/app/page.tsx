8a37909dfc20b7930fcdc0e85690a9a1edc00293
Tyler Treadwell <tyler@ttrds.com>
2023-08-18 00:53:06 -0400

    Refinements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -168,16 +168,15 @@ export default function Home() {
 
         <footer className='max-w-[540px] m-auto md:p-5 '>
           <div className='pb-10 pt-5'>
-            <div className="text-[12px]">Connect on <a target="_blank" className='underline' href='https://www.linkedin.com/in/ttreads/'>Linkedin</a></div>
             <div className='flex-col'><h3 className="text-[20px] font-bold tracking-tight text-gray-900 leading-tight"><span aria-label='ttrds. Tyler Treadwell Designs'>ttrds.</span></h3></div>
             <div className='md:flex justify-between'>
 
               <div className='flex-col text-[12px] mt-2 text-left'>
-                <p>Find me on <a target="_blank" className='underline' href="https://dribbble.com/ttreads">Dribbble</a></p>
-                <p>Browse my code on <a target="_blank" className='underline' href="https://github.com/ttreads">Github</a></p>
+                <p>Find me on <a target="_blank" className='underline' href='https://www.linkedin.com/in/ttreads/'>LinkedIn</a>, <a target="_blank" className='underline' href="https://dribbble.com/ttreads">Dribbble</a></p>
+                <p>Browse my code on <a target="_blank" className='underline' href="https://github.com/ttreads">GitHub</a></p>
               </div>
               <div className='flex-col text-[12px] mt-2 md:text-right'>
-                <p>Built using <a target='_blank' className='underline' href='https://nextjs.org/?via=TylerTreadwell'>NextJS</a></p>
+                <p>Built using <a target='_blank' className='underline' href='https://nextjs.org/?via=TylerTreadwell'>Next.js</a></p>
                 <p className='text-[12px]'>&copy; {Year} All Rights Reserved</p>
               </div>
 
